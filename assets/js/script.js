7e0d22a81235c5e59d44059f7e984e4f0b4687fb
Red3ffect <red3ffect117@gmail.com>
2021-06-13 13:14:05 -0700

    Adds initial html

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let searchFormEl = document.querySelector("#search-form");
+let cityInputEl = document.querySelector("#city-name");
+
+let formSubmitHandler = function (event) {
+    let city = cityInputEl.nodeValue.trim();
+}
